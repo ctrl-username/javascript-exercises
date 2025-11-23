e8ab5e9563659d9fbf63048c8782d35a6f8d2e0f
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2025-11-23 15:41:17 +0100

    solved 06_repeatString function

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,6 @@
-const repeatString = function() {
-
+const repeatString = function (text, num) {
+  return console.log(text.repeat(num));
 };
-
+repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;
