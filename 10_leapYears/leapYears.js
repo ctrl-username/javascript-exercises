270e8339b246730f564cfc009a7fe0f2024ff891
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2025-12-12 15:39:13 +0100

    finally got a solution to leap year .js

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,37 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  // leap year is divisible by 4
+  // century is not a leap year (divisible by 100)
+  // century divisible by 400 is leap year
+  // okay it works pretty well huh
+  //year % 4  === 0 ? true : false ||  year % 100 === 0 ? false : year % 400 === 0 ? true : false;
+  //
+  if (year % 400 == 0 && year % 100 != 0) {
+    return true;
+  }
 
+  if (year % 4 == 0 && year % 100 != 0) {
+    return true;
+  }
+
+  if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+    return true;
+  }
+  return false;
 };
 
+console.log(leapYears(1600));
 // Do not edit below this line
 module.exports = leapYears;
+
+// Is it divisible by 4? → If NO, it's NOT a leap year. Stop here.
+//Is it divisible by 100? → If NO, it IS a leap year. Stop here.
+//Is it divisible by 400? → If YES, it IS a leap year. If NO, it's NOT.
+// how are you doing
+//
+//
+//
+//
+//
+//
+//
+//
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahren) {
+  return Math.round((fahren - 32) * (5 / 9), 2);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  return Math.round(celsius * (9 / 5) + 32, 2);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
+  d,
 };
+
+console.log(convertToCelsius(33), convertToFahrenheit(33));
