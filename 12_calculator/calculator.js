a8f766579d8413eb4dd2db924699b306d80f91b0
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2025-12-12 16:48:13 +0100

    started working on 12_calculator.js

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (a, b) {
+  return a - b;
 };
 
-const multiply = function() {
-
+const multiply = function (a, b) {
+  return a * b;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  for (let i = n; i <= n; i++) {
+    sum *= n;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +31,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
