ed42e3912db08586bfe4afa538253cbd831ef01b
ctrl-username <243785975+ctrl-username@users.noreply.github.com>
2026-01-09 14:23:10 +0100

    done with fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+     if(n <=0) return 0;
+     if(n === 1 || n === 2) return 1;
+    let prev = 1, curr =1;
+    for(let i = 3; i <= n; i++){
+        let next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    return curr;
 
 };
 
